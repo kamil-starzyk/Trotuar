6e80cf7f0aa090d8e41bf61bc08c3877465db856
XesIhma <xesihma@o2.pl>
2023-10-26 23:38:02 +0200

    created title_screen function for mapping player's choice. Time for first run. I'm so excited xD

--- game.py
+++ game.py
@@ -2,6 +2,7 @@ from world import World
 from square import Square
 from mob import Mob
 from player import Player
+from konsola import Konsola
 from myjson import MyJson
 
 class Game:
@@ -10,8 +11,36 @@ class Game:
 		self.gameplay = 0
 		self.god_mode = True
 		self.world = None
-		self.hero = None
+		self.player = None
 		self.is_playing = False
 		self.time_in_sec = 0
 	
+	def title_screen(self):
+		method_map = {
+			"1": self.new_game,
+			"5": self.end_game,
+		}
+
+		Konsola.show_title_screen(self.version)
+		while not self.is_playing:
+			choice = input()
+			if choice in method_map:
+				method_map[choice]()
+			else:
+				print("Wprowadź poprawny wybór")
+
+
+	def new_game(self):
+		data = MyJson.load_json("data/init/world.json")
+		self.world = World.from_dict(data)
+		self.player = Player(0,0, "Jozin", "z Bazin", "hooman", 90, 100, 5, 5, 10, 10, 10, 0, 1, 90, 100, 70, 100)
+		self.is_playing = True
 	
+
+	def end_game(self):
+		self.is_playing = False
+		Konsola.print("Czy na pewno chcesz wyjść? Upewnij się, że zapisałeś grę (Y/N)", "lyellow", "red")
+		are_you_sure = input()
+		are_you_sure = "Y"
+		if are_you_sure in ("Y", "y"):
+			exit()
--- trotuar.py
+++ trotuar.py
@@ -1,1 +1,7 @@
 from game import Game
+
+game = Game()
+
+game.title_screen()
+
+print("Brawo! Udało Ci się rozpocząć grę")
