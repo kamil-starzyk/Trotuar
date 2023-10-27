c257a1d7c3b79bcd1d70d4ce2326efc0c20bb225
XesIhma <kamil_starzyk@outlook.com>
2023-10-27 18:42:07 +0200

    moved exits to dictionary and fixed dict methods as well as json file

--- square.py
+++ square.py
@@ -1,16 +1,18 @@
 class Square:
-  def __init__(self, x, y, z, name, description, exit_n, exit_e, exit_s, exit_w, exit_u, exit_d):
+  def __init__(self, x, y, z, name, description, exits):
     self.x = x
     self.y = y
     self.z = z
     self.name = name
     self.description = description
-    self.exit_n = exit_n
-    self.exit_e = exit_e
-    self.exit_s = exit_s
-    self.exit_w = exit_w
-    self.exit_u = exit_u
-    self.exit_d = exit_d
+    self.exits = {
+      "n": exits["n"],
+      "e": exits["e"],
+      "s": exits["s"],
+      "w": exits["w"],
+      "u": exits["u"],
+      "d": exits["d"]
+    }
   
   def to_dict(self):
     return {
@@ -19,15 +21,9 @@ class Square:
       "z": self.z,
       "name": self.name,
       "description": self.description,
-      "exit_n": self.exit_n,
-      "exit_e": self.exit_e,
-      "exit_s": self.exit_s,
-      "exit_w": self.exit_w,
-      "exit_u": self.exit_u,
-      "exit_d": self.exit_d
+      "exits": self.exits
     }
   
   @classmethod
   def from_dict(cls, data):
-    return cls(data["x"], data["y"], data["z"], data["name"], data["description"], 
-               data["exit_n"], data["exit_e"], data["exit_s"], data["exit_w"], data["exit_u"], data["exit_d"])
+    return cls(data["x"], data["y"], data["z"], data["name"], data["description"], data["exits"])
--- trotuar.py
+++ trotuar.py
@@ -3,5 +3,6 @@ from game import Game
 game = Game()
 
 game.title_screen()
+print(game.world.locations[0].squares[0].exits["n"])
 
 print("Brawo! Udało Ci się rozpocząć grę")
